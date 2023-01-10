9ba657d4be5475a8d720f725d5ad46d9505ce2e0
severmost <116316277+severmost@users.noreply.github.com>
2023-01-10 08:04:55 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+//МЕНЮ
+
+const menuToggle = document.querySelector('.menu-toggle')
+const menu = document.querySelector('.menu-container ul')
+const prevBtn = document.querySelector('.prevbtn')
+const nextBtn = document.querySelector('.nextbtn')
+const servicebox = document.querySelector('.services')
+const itemWidth = servicebox.querySelector('.service').clientWidth
+
+menuToggle.addEventListener('click', function () {
+  menu.classList.toggle('active')
+  this.classList.toggle('active')
+})
+
+window.onscroll = function () {
+  const scrolled = window.pageYOffset || document.documentElement.scrollTop // Получаем положение скролла
+  if (scrolled !== 0) {
+    // Если прокрутка есть, то делаем блок прозрачным
+    document.querySelector('header').style.backgroundColor = '#222'
+    document.querySelector('header').style.opacity = '0.9'
+  } else {
+    // Если нет, то делаем его полностью видимым
+    document.querySelector('header').style.backgroundColor = 'rgba(0, 0, 0, 0)'
+    document.querySelector('header').style.opacity = '1'
+  }
+}
+
+nextBtn.addEventListener('click', scrollToNextItem)
+prevBtn.addEventListener('click', scrollToPrevItem)
+
+function scrollToNextItem() {
+  servicebox.scrollBy({ left: itemWidth, top: 0, behavior: 'smooth' })
+}
+function scrollToPrevItem() {
+  servicebox.scrollBy({ left: -itemWidth, top: 0, behavior: 'smooth' })
+}
+
+const prices = document.querySelectorAll('.price-header')
+const pricesDescr = document.querySelectorAll('.description-p')
+
+prices.forEach((price, i) => {
+  console.log(i)
+  price.addEventListener('click', () => changePrice(i, price))
+})
+
+function changePrice(i, price) {
+  prices.forEach((_price) => {
+    _price.classList.remove('active-price')
+  })
+  price.classList.toggle('active-price')
+  pricesDescr.forEach((descr) => descr.classList.add('none'))
+  pricesDescr[i].classList.remove('none')
+}
