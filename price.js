caf43e9c4a23e7d5852fca05364f1046686974d6
severmost <116316277+severmost@users.noreply.github.com>
2023-01-17 08:43:01 +0600

    Add files via upload

--- price.js
+++ price.js
@@ -1,20 +1,43 @@
 const container = document.querySelector('.price-container')
 const priceEl = container.querySelector('.full-price')
+const btnWhatsUp = priceEl.querySelector('.full-price-btn')
 let fullPrice = 0
+let texts = []
 container.addEventListener('click', (e) => {
-  e.preventDefault()
+  let price, selected
   if (e.target.firstElementChild) {
-    let selected = e.target.firstElementChild.classList
-    if (selected.contains('selected') && selected.contains('none')) {
+    price = e.target
+    selected = price.firstElementChild.classList
+    if (e.target.classList.contains('price-text')) {
+      price = e.target.parentElement
+      selected = price.firstElementChild.classList
+    }
+
+    if (price.classList.contains('price') && selected.contains('none')) {
       selected.toggle('none')
-      fullPrice += +e.target.dataset.price
+      fullPrice += +price.dataset.price
+      let textprice = price.querySelectorAll('p')
+      texts.push(`${textprice[0].textContent} за ${textprice[1].textContent}`)
     }
   } else if (e.target.classList.contains('selected')) {
     e.target.classList.toggle('none')
+    let delText = e.target.parentElement.querySelectorAll('p')
+    delText = `${delText[0].textContent} за ${delText[1].textContent}`
+    texts = texts.filter((item) => {
+      return item !== delText
+    })
     fullPrice -= +e.target.parentElement.dataset.price
   }
   priceEl.firstElementChild.innerHTML = `Итоговая цена: <b style="font-size: 1.5rem">${fullPrice}</b> тг`
-  console.log(priceEl.firstChild)
-  console.log(e.target)
-  console.log(fullPrice)
+  let textForLink = ''
+  texts.forEach((item, i) => {
+    for (let j = 0; j < item.length; j++) {
+      if (item[j] === ' ') {
+        textForLink += '%20'
+      } else textForLink += item[j]
+    }
+    textForLink += '%0A'
+  })
+  console.log(textForLink)
+  btnWhatsUp.href = `https://wa.me/77076821729?text=Здравствуйте.%20Интересует%20следующий%20пакет%20услуг:%0A${textForLink}Итоговая%20цена%20на%20сайте%20вышла%20в%20${fullPrice}тг.`
 })
